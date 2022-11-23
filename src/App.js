edeece080f1212510faec8368d468de1db56ef13
Amit Kumar Thakur <kingston.amit9830@gmail.com>
2022-11-23 05:51:03 +0530

    adding class componenets

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import './App.css';
 
 import Welcome from './components/Welcome';
+import ClassComponets from './components/ClassComponents';
 
 function App() {
   return (
     <div className="App">
       <Welcome />
+      <ClassComponets />
     </div>
   );
 }
---
+++ src/components/ClassComponents.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Class_componets extends React.Component{
+    render(){
+        return <h1>This is Class Components </h1>
+    }
+}
+
+export default Class_componets;
