e3da334dd7c196598cfa3eaa9b9639b07c070994
Amit Kumar Thakur <kingston.amit9830@gmail.com>
2022-11-23 05:46:04 +0530

    function components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 
 import './App.css';
 
-
+import Welcome from './components/Welcome';
 
 function App() {
   return (
     <div className="App">
-      
+      <Welcome />
     </div>
   );
 }
---
+++ src/components/Welcome.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Welcome = () => {
+  return (
+    <div>Welcome To Learing React</div>
+  )
+}
+
+export default Welcome
